efb8fa2c33ac25332f0bcbf7ce95d0ba32c45fcf
Sahil <cssahil98@outlook.com>
2021-05-08 17:39:25 +0530

    Update Multiple-Inheritence.java

--- Inheritence/Multiple-Inheritence.java
+++ Inheritence/Multiple-Inheritence.java
@@ -1,31 +1,36 @@
-class Sports{
-    String type="Sports"; 
-    String where="Stadium";
-
-    void does(){ 
-    System.out.println("Playing");
-    }
+class Employe{  
+    float salary=40000;  
+   }  
+class Programmer extends Employe
+{  
+  int bonus=10000;  
+ }
+class Intern extends Programmer
+{
+	int stipend=2300;
 }
+/*class Boss extends Employe,Programmer,Intern
+{
+	int turnover=5600000;
+}*/
+//since multiple inheritance not supported in JAVA
+class Main
+{
+public static void main(String[] args){  
+Programmer p=new Programmer();  
+System.out.println("Programmer salary is:"+p.salary);  
+System.out.println("Bonus of Programmer is:"+p.bonus);
 
-class Cricket extends Sports{ 
-    String sporttype="Cricket";
-}
+Intern aryan=new Intern();
+System.out.println("Aryan's salary:"+aryan.salary);
+System.out.println("Aryan's bonus:"+aryan.bonus);
+System.out.println("Aryan's stipend:"+aryan.stipend);
 
-
-class Player extends Cricket{ 
-    String name = "Dhoni";
-    String number = "7";
-}
-
-class Main{
-    public static void main(String[] args) { 
-        Player p = new Player(); 
-        System.out.println(p.name); 
-        System.out.println(p.number); 
-        System.out.println(p.sporttype); 
-        System.out.println(p.type); 
-        System.out.println(p.where); 
-        
-        p.does();
-    }
+/*
+Boss b=new Boss();
+System.out.println("b's salary:"+b.salary);
+System.out.println("b's bonus:"+b.bonus);
+System.out.println("b's stipend:"+b.stipend);
+System.out.println("b's stipend:"+b.turnover);*/
+}  
 }
