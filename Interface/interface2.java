89cf7ac044dc631294350211bf234b22bf969a1c
Sahil <cssahil98@outlook.com>
2021-05-08 17:46:33 +0530

    Update interface2.java

--- Interface/interface2.java
+++ Interface/interface2.java
@@ -1,25 +1,24 @@
-interface Math{
-    void add(int a, int b); 
-    void subtract(int a, int b);
+interface FirstInterface {
+  public void myMethod(); 
 }
 
-class Operate implements Math{
-    public void add(int a, int b)
-    {
-        System.out.println(a + b);
-    }
-
-    public void subtract(int a, int b)
-    { 
-        System.out.println(a - b);
-    }
+interface SecondInterface {
+  public void myOtherMethod(); 
 }
 
-class Main{
-    public static void main(String[] args)
-    {     
-        Operate obj = new Operate(); 
-        obj.add(10, 20);
-        obj.subtract(50, 10);
-    }
+class DemoClass implements FirstInterface, SecondInterface {
+  public void myMethod() {
+    System.out.println("Some text..");
+  }
+  public void myOtherMethod() {
+    System.out.println("Some other text...");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    DemoClass myObj = new DemoClass();
+    myObj.myMethod();
+    myObj.myOtherMethod();
+  }
 }
