604881e5cf0dd5500383349572544064438e6767
Sahil <cssahil98@outlook.com>
2021-05-08 17:45:47 +0530

    Update interface1.java

--- Interface/interface1.java
+++ Interface/interface1.java
@@ -1,21 +1,21 @@
-interface Person {
-    void getName(String name);
+interface Animal {
+  public void animalSound(); 
+  public void sleep(); 
+
+
+class Pig implements Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+  public void sleep() {
+    System.out.println("Zzz");
+  }
 }
-
-
-class PersonName implements Person {
-    public void getName(String name)
-    {
-        System.out.println("Person Name: " + name); 
-        System.out.println("He Is The Richest Person In The World");
-    }
-}
-
 
 class Main {
-    public static void main(String[] args)  
-    { 
-        PersonName person = new PersonName(); 
-        person.getName("Elon Musk");
-    }
+  public static void main(String[] args) {
+    Pig myPig = new Pig();  
+    myPig.animalSound();
+    myPig.sleep();
+  }
 }
